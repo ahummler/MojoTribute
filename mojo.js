faf4c0b7dbc212bc55e0002c23324155136866de
Arthur <phillysuperbowl@gmail.com>
2016-05-30 12:01:27 -0400

    fade in code

---
+++ mojo.js
@@ -0,0 +1,20 @@
+// window.alert("sometext");
+
+$(document).ready(function () {
+    $('.div').fadeIn(1000).removeClass('mojo');
+});
+
+
+// $(document).ready(function() {
+//     $('div').fadeOut(1);
+//     $('div').removeClass('hidden');
+//     $('div').fadeIn(1000);
+// });
+
+// $(function() {
+// $('#test').fadeOut(100, function() {
+//     $(this).text('Bunny').fadeIn(100);
+// });
+//  $('#test').fadeOut(100, function() {
+//     $(this).text('Turtle').fadeIn(100);
+// });
