a71382f9e2e5608b6f984360d1418f3f2e781225
Arthur <phillysuperbowl@gmail.com>
2016-05-30 15:38:03 -0400

    1st commit

---
+++ mojo.js
@@ -0,0 +1,35 @@
+// window.alert("sometext");
+
+// if (typeof jQuery != 'undefined') {
+
+//     alert("jQuery library is loaded!");
+
+// }else{
+
+//     alert("jQuery library is not found!");
+
+// }
+$(window).load(function() {
+   $('div').each(function(i) {
+      $(this).delay((i + 1) * 1250).fadeIn(5000,function() { $('span').fadeOut(35000,function() { $('div').fadeOut(7000,function() { $('nav').fadeIn(3000); }); }); });
+   });
+});
+
+// $(document).ready(function () {
+//     $('div').fadeIn(10000).removeClass('mojo');
+// });
+
+
+// $(document).ready(function() {
+//     $('div').fadeOut(1);
+//     $('div').removeClass('hidden');
+//     $('div').fadeIn(1000);
+// });
+
+// $(function() {
+// $('#test').fadeOut(100, function() {
+//     $(this).text('Bunny').fadeIn(100);
+// });
+//  $('#test').fadeOut(100, function() {
+//     $(this).text('Turtle').fadeIn(100);
+// });
